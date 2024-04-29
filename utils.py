d15c77f89754e1eaec45bbb9858ed61a554ddead
henriquebrnetto <henriquebrnetto02@gmail.com>
2024-04-29 17:42:42 -0300

    Update utils.py
    Co-Authored-By: Lucas ikawa <123121902+Lucasikawa@users.noreply.github.com>

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from os import listdir, getcwd
 from os.path import dirname
 import json, requests, urllib
+import urllib.parse
 from collections import Counter
 import pymongo
 from geopy.distance import geodesic
@@ -20,8 +21,12 @@ def conn_mongo():
     client = pymongo.MongoClient('mongodb+srv://henriquebrnetto02:2K4y7AIS4IOddUkC@menu.cyvtolc.mongodb.net/?retryWrites=true&w=majority&appName=MENU')
     return client, client['MENU']
 
-def busca_loc(address):
-    link = 'https://geocode.search.hereapi.com/v1/geocode?q=' + urllib.parse.quote_plus(address) + '+Sao+Paulo&apiKey=WcO15K23iW5HkYEnLSuFq15vXjzq_f8VhHU4E66pr6E'
+def busca_loc(address=''):
+    if type(address) == int:
+        end = requests.get(f'viacep.com.br/ws/{address}/json/')
+        address = end['logradouro']+end['localidade']
+
+    link = f'https://geocode.search.hereapi.com/v1/geocode?q={urllib.parse.quote_plus(address)}&apiKey=WcO15K23iW5HkYEnLSuFq15vXjzq_f8VhHU4E66pr6E'
     val = requests.get(link)
     return json.loads(val.text)['items'][0]['position']
 
@@ -33,9 +38,12 @@ def calcula_dist(user_loc, rest_loc):
     if type(rest_loc) == dict:
         rest_loc = (rest_loc['lat'], rest_loc['lng'])
 
-    url = f'https://router.hereapi.com/v8/routes?transportMode=car&origin={user_loc[0]},{user_loc[1]}&destination={rest_loc[0]},{rest_loc[1]}&return=travelSummary,polyline&spans=streetAttributes,length,segmentRef&apikey={api_key}'
-    return requests.get(url).json()
+    url_pedestre = f'https://router.hereapi.com/v8/routes?transportMode=pedestrian&origin={user_loc[0]},{user_loc[1]}&destination={rest_loc[0]},{rest_loc[1]}&return=travelSummary&apikey={api_key}'
+    dist_pedestre = requests.get(url_pedestre).json()['routes'][0]['sections'][0]['travelSummary']['length']
 
-print(calcula_dist(busca_loc('Rua Dr. Renato Paes de Barros 283'), busca_loc('R. Pedroso Alvarenga, 365')))
+    url_carro = f'https://router.hereapi.com/v8/routes?transportMode=car&origin={user_loc[0]},{user_loc[1]}&destination={rest_loc[0]},{rest_loc[1]}&return=travelSummary&apikey={api_key}'
+    dist_carro = requests.get(url_carro).json()['routes'][0]['sections'][0]['travelSummary']['length']
 
+    return {'carro' : dist_carro, 'pedestre' : dist_pedestre}
 
+print(calcula_dist(busca_loc('1901 S Atlantic Ave, Daytona Beach', 'Orlando'), busca_loc('200 Rialto PI, Melbourne', 'Orlando')))
